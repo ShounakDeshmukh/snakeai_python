955a4fa1a8dfcc086ad3e498bac29e1ac5786448
Shounak <shounsach@gmail.com>
2023-06-16 16:29:37 +0530

    machine interactive game no user input

--- snake_game_ai.py
+++ snake_game_ai.py
@@ -2,6 +2,7 @@ import pygame
 import random
 from enum import Enum
 from collections import namedtuple
+import numpy as np
 
 pygame.init()
 font = pygame.font.Font(None, 25)
@@ -27,15 +28,15 @@ CATPPUCCIN_BLACK = (17, 17, 27)
 WINDOW_H = 480
 WINDOW_W = 640
 
+POSITIVE_REWARD = 10
+NEGATIVE_REWARD = -10
+
 
 class SnakeGameAI:
     def __init__(self):
         pygame.display.set_caption("SnakeAI")
         self.display = pygame.display.set_mode((WINDOW_W, WINDOW_H))
         self.reset()
-        self.frame_count = 0
-
-        self.place_food()
 
     def place_food(self):
         x = (
@@ -51,6 +52,7 @@ class SnakeGameAI:
             self.place_food()
 
     def reset(self):
+        self.food = None
         self.direction = Direction.RIGHT
         self.snake_head = Point(WINDOW_W / 2, WINDOW_H / 2)
         self.snake = [
@@ -59,8 +61,26 @@ class SnakeGameAI:
             Point(self.snake_head.x - (2 * BODY_SEGMENT_SIZE), self.snake_head.y),
         ]
         self.score = 0
+        self.frame_count = 0
+        self.place_food()
 
-    def move(self):
+    def move(self, action):
+        # [Straight, right, left] one hotr encoded
+
+        clock_wise = [Direction.RIGHT, Direction.DOWN, Direction.LEFT, Direction.UP]
+        index = clock_wise.index(self.direction)
+
+        if np.array_equal(action, [1, 0, 0]):
+            new_dir = clock_wise[index]  # no change
+        elif np.array_equal(action, [0, 1, 0]):
+            next_idx = (index + 1) % 4
+            new_dir = clock_wise[next_idx]  # right turn r -> d -> l -> u
+        else:  # [0, 0, 1]
+            next_idx = (index - 1) % 4
+            new_dir = clock_wise[next_idx]  # left turn r -> u -> l -> d
+
+        self.direction = new_dir
+
         x = self.snake_head.x
         y = self.snake_head.y
         if self.direction == Direction.RIGHT:
@@ -73,49 +93,45 @@ class SnakeGameAI:
             y -= BODY_SEGMENT_SIZE
         self.snake_head = Point(x, y)
 
-    def is_collision(self):
+    def is_collision(self, pt=None):
+        if pt is None:
+            pt = self.snake_head
         if (
-            self.snake_head.x > WINDOW_W - BODY_SEGMENT_SIZE
-            or self.snake_head.x < 0
-            or self.snake_head.y > WINDOW_H - BODY_SEGMENT_SIZE
-            or self.snake_head.y < 0
+            pt.x > WINDOW_W - BODY_SEGMENT_SIZE
+            or pt.x < 0
+            or pt.y > WINDOW_H - BODY_SEGMENT_SIZE
+            or pt.y < 0
         ):
             return True
-        if self.snake_head in self.snake[1:]:
+        if pt in self.snake[1:]:
             return True
         return False
 
     def game_tick(self):
+        self.frame_count += 1
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return True, self.score
-            if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_LEFT and self.direction != Direction.RIGHT:
-                    self.direction = Direction.LEFT
-                elif event.key == pygame.K_RIGHT and self.direction != Direction.LEFT:
-                    self.direction = Direction.RIGHT
-                elif event.key == pygame.K_UP and self.direction != Direction.DOWN:
-                    self.direction = Direction.UP
-                elif event.key == pygame.K_DOWN and self.direction != Direction.UP:
-                    self.direction = Direction.DOWN
 
         self.move()
         self.snake.insert(0, self.snake_head)
-
+        reward = 0
         is_game_over = False
-        if self.is_collision():
+        if self.is_collision() or self.frame_count > 1000 * len(self.snake):
             is_game_over = True
+            reward = NEGATIVE_REWARD
+            return reward, is_game_over, self.score
+
         if self.snake_head == self.food:
             self.score += 1
+            reward = POSITIVE_REWARD
             self.place_food()
         else:
             self.snake.pop()
-        if self.is_collision():
-            is_game_over = True
         self.draw_frames()
         pygame.time.Clock().tick(SPEED)
 
-        return is_game_over, self.score
+        return reward, is_game_over, self.score
 
     def draw_frames(self):
         self.display.fill(CATPPUCCIN_BLACK)
@@ -143,12 +159,3 @@ class SnakeGameAI:
         text = font.render("Score: " + str(self.score), True, CATPPUCCIN_WHITE)
         self.display.blit(text, [0, 0])
         pygame.display.flip()
-
-
-if __name__ == "__main__":
-    snake_game = SnakeGameAI()
-    is_game_over = False
-    while not is_game_over:
-        is_game_over, score = snake_game.game_tick()
-    print("Final Score:", score)
-    pygame.quit()
